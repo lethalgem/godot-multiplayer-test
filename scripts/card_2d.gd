2c99bdce0b4f54e7db8d780376a554e5a21b7dc1
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-07-24 18:43:46 -0500

    Added Collision and Texture

--- scripts/card_2d.gd
+++ scripts/card_2d.gd
@@ -1,8 +1,7 @@
 class_name Card2D extends Sprite2D
 
 @export var area_2d: Area2D
-@export var collision_2d: CollisionShape2D
+@export var collision_2d: CollisionPolygon2D
 
 func _ready():
-	area_2d.
 	pass
