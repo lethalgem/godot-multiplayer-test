891dfa003696e2e871fe5d7280d4534b2cfee9e7
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-07-24 19:24:46 -0500

    Card Update  RayCast

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -1,5 +1,15 @@
 class_name MainGame extends Node2D
 
 func _input(event):
-	if event is InputEventMouseButton:
-		pass
+	if event.is_action_pressed('left_click'):
+		raycast_check_for_card()
+		print("Click Left Button")
+
+func raycast_check_for_card():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = 1
+	var result = space_state.intersect_point(parameters)
+	print(result)
