788da9451b60b61dcfe4bf7c81a37146b1bdc008
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-07-28 20:35:14 -0500

    Detect Hovering Over Cards

--- scripts/card_2d.gd
+++ scripts/card_2d.gd
@@ -3,5 +3,15 @@ class_name Card2D extends Sprite2D
 @export var area_2d: Area2D
 @export var collision_2d: CollisionPolygon2D
 
+signal card_hover_active
+signal card_hover_inactive
+
 func _ready():
-	pass
+	#This will get card manager
+	get_parent().connect_card_signals(self)
+
+func _on_area_2d_mouse_entered():
+	emit_signal("card_hover_active", self)
+
+func _on_area_2d_mouse_exited():
+	emit_signal("card_hover_inactive", self)
